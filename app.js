a5c97a14fe81eae36d5847cda2a15c6908a9a9b1
Greg W <gregw1@iname.com>
2016-11-21 01:16:11 -0800

    Added public HTML files and made more changes to app.js and test.js

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 var express = require('express')
 var app = express();
 
-// Create a route for the root path
-app.get('/', function(req, res) {
-   res.send('OK');	
+app.use(express.static('public')); // Mount the static middleware, passing in the folder
+                                   // where our static files are located
+
+// Create a route for the cities path
+app.get('/cities', function(req, res) {
+   var cities = ['Lotopia', 'Caspiana', 'Indigo'];
+   res.json(cities); // use json instead of send to force into json format	
 });
 
 module.exports = app;
--- test.js
+++ test.js
@@ -1,15 +1,47 @@
 var request = require('supertest');
 var app = require('./app');
 
+// A simple supertest/mocha test wrapper (describe-it)
 describe('Requests to the root path', function() {
   it('Returns a 200 status code', function(done) {
     request(app)
     .get('/')
     .expect(200)
-    .end(function(err){
+    .end(function(err) {
        if (err) throw err;
        done();
     });
   })
+
+  it('Returns an HTML format', function(done) {
+    request(app)
+    .get('/')
+    .expect('Content-Type', /html/, done) // check for HTML via a regex
+  })
+
+  it('Returns an index file with cities', function(done) {
+    request(app)
+    .get('/')
+    .expect(/cities/i, done) // check for generic Cities index file
+  })
 })
 
+describe('Listing cities on /cities', function() {
+  it ('Returns 200 status code', function(done) {
+    request(app)
+    .get('/cities')
+    .expect(200, done); // done is being used as a shortcut here; equivalent to .end function above
+  })
+
+  it ('Returns JSON format', function(done) {
+    request(app)
+    .get('/cities')
+    .expect('Content-Type', /json/, done) // check for json via a regex
+  })
+
+  it ('Returns initial cities', function(done) {
+    request(app)
+    .get('/cities')
+    .expect(JSON.stringify(['Lotopia', 'Caspiana', 'Indigo']), done)
+  })
+})
