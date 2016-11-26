0cc8c3a179823aa82112cec82a347eae6619ea32
Greg W <gregw1@iname.com>
2016-11-26 14:56:18 -0800

    changed app.js to get client website working and added favicon in index.html

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 var express = require('express')
 var app = express();
 
-app.use(express.static('public')); // Mount the static middleware, passing in the folder
-                                   // where our static files are located
+// This works but the version below it does not
+app.use(express.static(__dirname + '/public'));  // Mount the static middleware, passing in the
+						 // folder where our static files are located
+
+// Doesn't work for some reason
+//app.use(express.static('public')); // Mount the static middleware, passing in the folder
+                                     // where our static files are located
 
 // Create a route for the cities path
 app.get('/cities', function(req, res) {
